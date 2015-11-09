a27750c2b57c3d8a422d680d90b4e33a94068937
Yelena Denezhnaya <elenamelicenta@gmail.com>
2015-11-09 10:14:20 +0300

    initial demo

--- public/javascripts/preload.js
+++ public/javascripts/preload.js
@@ -3,6 +3,14 @@
  */
 $(window).load(function (){
     var b = $('body');
+    var img = $('.bc');
+    $('.preloader').css('display','block');
+    $.ajax({
+        url:'/images/RxSUX.png',
+        context: document.body
+    }).done(function (){
+        b.css('background-image','url(/images/RxSUX.png)');
+        $('.preloader').css('display','none');
+    });
 
-    b.css('background-image','  url("/images/RxSUX.png")');
-})
+});
