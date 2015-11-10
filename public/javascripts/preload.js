2646322c5d8f5383f23f16c6a952b79f25d999bd
Yelena Denezhnaya <elenamelicenta@gmail.com>
2015-11-10 10:13:09 +0300

    initial demo

--- public/javascripts/preload.js
+++ public/javascripts/preload.js
@@ -1,17 +1,41 @@
 /**
  * Created by denezhnaya on 06.11.2015.
  */
-$(function (){
-    var b = $('body');
-    var img = $('.bc');
-    $('.preloader').css('display','block');
-    $.ajax({
-        method: 'GET',
-        url:'/',
-        context: document.body
-    }).done(function (){
-        b.css('background-image','url(/images/RxSUX.png)');
-        $('.preloader').css('display','none');
-    });
+var script = document.createElement('script');
+script.src = "/javascripts/jquery-ui-1.11.4/external/jquery/jquery.js";
+document.head.appendChild(script);
 
-});
+function afterLoad() {
+    $('.preloader').css('display', 'block');
+    alert("Загрузка завершена: " + typeof(jQuery));
+    var b = $('body');
+    var img = document.createElement('img');
+
+    img.onload = function () {
+        alert("Успех " + this.src);
+        //b.css('background-image','url(this.src)');
+        $('.preloader').css('display','none');
+    };
+
+    img.onerror = function () {
+        alert("Ошибка " + this.src);
+    }
+    img.src = '/images/RxSUX.png'
+}
+
+script.onload = script.onerror = function() {
+    if (!this.executed) { // выполнится только один раз
+        this.executed = true;
+        afterLoad();
+    }
+};
+
+script.onreadystatechange = function() {
+    var self = this;
+    if (this.readyState == "complete" || this.readyState == "loaded") {
+        setTimeout(function() {
+            self.onload()
+        }, 0); // сохранить "this" для onload
+    }
+};
+
